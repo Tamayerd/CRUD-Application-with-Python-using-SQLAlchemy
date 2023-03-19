a8d08d91e85588523266f64531b90be0b430f4de
Tamayerd <127382070+Tamayerd@users.noreply.github.com>
2023-03-19 15:53:46 +0300

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,6 +1,5 @@
 from school import Student, session, engine
 
-#veri tabanı ile iletişim sağlanıyor.
 local_session = session(bind = engine)
 
 #Veri tabanından istenilen verinin id'si girilerek bir query filtresi oluşturulur ve session yardımıyla bu veri silinir
